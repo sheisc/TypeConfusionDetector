448cbc2328cb71dfdefe02560e06575ef830e5ff
sheisc <sheisc@163.com>
2022-05-17 05:16:28 +1000

    FIX build.sh

--- build.sh
+++ build.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-#mkdir -p ${R4PATH}/build
-#cd ${R4PATH}/build
-#cmake ../SVF7.0
-#make
-
-# cd ${R4PATH}/AsmRewriter
-# make
-
-# sudo apt install python-pip
-# sudo pip install wllvm
-# sudo apt-get install gcc-multilib g++-multilib
-# sudo apt-get install git
-
-
 LLVM_BUILD_DIR=llvm-build
 
 
@@ -33,6 +19,7 @@ SVF_BUILD_DIR=svf-build
 
 if [ ! -d $SVF_BUILD_DIR ]; then
 	mkdir -p $SVF_BUILD_DIR
+	cd $SVF_BUILD_DIR
 	cmake ../SVF-7.0.0
 	make -j4
 	cd ..
--- env.sh
+++ env.sh
@@ -9,10 +9,5 @@ export LLVM_COMPILER=clang
 export LLVM_DIR=$LLVM_INSTALL_PATH
 export LD_LIBRARY_PATH=$LLVM_INSTALL_PATH/lib/
 
-
  
-# iron@CSE:~$ cd github/TypeConfusionDetector/
-# iron@CSE:TypeConfusionDetector$ cd svf-build/
-# iron@CSE:svf-build$ cd ..
-# iron@CSE:TypeConfusionDetector$ . ./env.sh 
-# iron@CSE:TypeConfusionDetector$ cd svf-build/
+
